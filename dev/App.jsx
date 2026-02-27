74f3da8a54c5ee5e75e0cbf490346433d55fd8d8
Jeanne Viallard <viallard.jeanne@gmail.com>
2026-02-27 13:57:05 +0100

    Implement first basic working version

--- dev/App.jsx
+++ dev/App.jsx
@@ -1,3 +1,21 @@
+import { useState } from "react";
+import { MenuSelect } from "../lib/index";
+
 export default function App() {
-  return <p>Hello World</p>;
+  const options = [
+    { text: "Sales", value: "Sales" },
+    { text: "Marketing", value: "Marketing" },
+    { text: "Engineering", value: "Engineering" },
+    { text: "Human Resources", value: "Human Resources" },
+    { text: "Legal", value: "Legal" },
+  ];
+  const [selectedOption, setSelectedOption] = useState(options[0]);
+
+  return (
+    <MenuSelect
+      options={options}
+      selectedOption={selectedOption}
+      setSelectedOption={setSelectedOption}
+    />
+  );
 }
--- lib/MenuSelect.jsx
+++ lib/MenuSelect.jsx
@@ -1,3 +1,48 @@
-export default function MenuSelect() {
-  return <p>Menu Select</p>;
+import { useState } from "react";
+
+export default function MenuSelec({ options, selectedOption, setSelectedOption }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleBtnClick() {
+    setIsOpen((prevValue) => !prevValue);
+  }
+
+  function handleOptionClick(option) {
+    setSelectedOption(option);
+    setIsOpen(false);
+  }
+
+  return (
+    <div>
+      <button
+        onClick={handleBtnClick}
+        aria-expanded={isOpen}
+        aria-haspopup="listbox"
+        aria-controls="listbox"
+        type="button"
+        role="combobox"
+        data-value={selectedOption.value}
+      >
+        {selectedOption.text}
+      </button>
+      {isOpen && (
+        <div role="listbox" id="listbox">
+          {options.map((option, index) => {
+            return (
+              <div
+                role="option"
+                aria-selected={selectedOption.value === option.value}
+                data-value={option.value}
+                id={`${index}-${option.value}`}
+                key={`${index}-${option.value}`}
+                onClick={() => handleOptionClick(option)}
+              >
+                {option.text}
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
 }
