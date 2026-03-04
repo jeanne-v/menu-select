abddc00cea6af33ce2409c9e954f14f7d40ae147
Jeanne Viallard <viallard.jeanne@gmail.com>
2026-03-04 10:22:39 +0100

    Add styles + enable keyboard navigation

--- lib/MenuSelect.jsx
+++ lib/MenuSelect.jsx
@@ -1,7 +1,17 @@
 import { useState, useEffect, useRef } from "react";
+import "./index.css";
+import chevron from "./assets/chevron.svg";
 
-export default function MenuSelec({ options, selectedOption, setSelectedOption }) {
+export default function MenuSelec({
+  options,
+  selectedOption,
+  setSelectedOption,
+  labelledby = null,
+}) {
   const [isOpen, setIsOpen] = useState(false);
+  const [focusedOptionIndex, setFocusedOptionIndex] = useState(
+    options.findIndex((option) => option.value === selectedOption.value),
+  );
   const wrapperRef = useRef(null);
 
   function handleBtnClick() {
@@ -11,6 +21,10 @@ export default function MenuSelec({ options, selectedOption, setSelectedOption }
   function handleOptionClick(option) {
     setSelectedOption(option);
     setIsOpen(false);
+  }
+
+  function handleOptionMouseEnter(optionIndex) {
+    setFocusedOptionIndex(optionIndex);
   }
 
   function handleDocumentClick(e) {
@@ -23,8 +37,28 @@ export default function MenuSelec({ options, selectedOption, setSelectedOption }
   function handleKeyDown(e) {
     const key = e.key;
 
-    if (key === "Escape" && isOpen) {
+    if (isOpen && key === "Escape") {
       setIsOpen(false);
+    } else if (isOpen && key === "Enter") {
+      setSelectedOption(options[focusedOptionIndex]);
+    } else if (isOpen && key === " ") {
+      setSelectedOption(options[focusedOptionIndex]);
+    } else if (isOpen && key === "ArrowUp") {
+      handleArrowKeyDownWhenOpen("ArrowUp");
+    } else if (isOpen && key === "ArrowDown") {
+      handleArrowKeyDownWhenOpen("ArrowDown");
+    } else if (!isOpen && key === "ArrowUp") {
+      setIsOpen(true);
+    } else if (!isOpen && key === "ArrowDown") {
+      setIsOpen(true);
+    }
+  }
+
+  function handleArrowKeyDownWhenOpen(arrowKey) {
+    if (arrowKey === "ArrowUp" && focusedOptionIndex !== 0) {
+      setFocusedOptionIndex((prevValue) => prevValue - 1);
+    } else if (arrowKey === "ArrowDown" && focusedOptionIndex < options.length - 1) {
+      setFocusedOptionIndex((prevValue) => prevValue + 1);
     }
   }
 
@@ -34,29 +68,41 @@ export default function MenuSelec({ options, selectedOption, setSelectedOption }
   }, []);
 
   return (
-    <div ref={wrapperRef} onKeyDown={handleKeyDown}>
+    <div ref={wrapperRef} onKeyDown={handleKeyDown} className="relative">
       <button
         onClick={handleBtnClick}
         aria-expanded={isOpen}
         aria-haspopup="listbox"
         aria-controls="listbox"
+        aria-activedescendant={isOpen ? options[focusedOptionIndex].value : null}
+        aria-labelledby={labelledby ? labelledby : null}
         type="button"
         role="combobox"
         data-value={selectedOption.value}
+        className={`p-2 cursor-pointer w-full flex justify-between items-center h-10 border border-light-middle-grey ${isOpen ? "rounded-t-sm" : "rounded-sm"}`}
       >
-        {selectedOption.text}
+        <span>{selectedOption.text}</span>
+        <img alt="" src={chevron} className={`h-4 ${isOpen ? "rotate-180" : ""}`} />
       </button>
       {isOpen && (
-        <div role="listbox" id="listbox">
+        <div
+          role="listbox"
+          id="listbox"
+          className="border-l border-r border-b border-light-middle-grey absolute left-0 top-10 bg-white w-full rounded-b-sm"
+        >
           {options.map((option, index) => {
+            const isSelectedOption = selectedOption.value === option.value;
+            const isFocusedOption = options[focusedOptionIndex].value === option.value;
             return (
               <div
                 role="option"
-                aria-selected={selectedOption.value === option.value}
+                aria-selected={isSelectedOption}
                 data-value={option.value}
-                id={`${index}-${option.value}`}
+                id={option.value}
                 key={`${index}-${option.value}`}
                 onClick={() => handleOptionClick(option)}
+                onMouseEnter={() => handleOptionMouseEnter(index)}
+                className={`p-2 cursor-pointer ${isFocusedOption ? "bg-blue text-white" : ""}`}
               >
                 {option.text}
               </div>
--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,13 @@ import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   build: {
     lib: {
       entry: resolve(__dirname, "lib/index.js"),
